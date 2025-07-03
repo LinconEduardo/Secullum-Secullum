32c76640fa614d1498d1cbe96b47da8803aa089f
LinconEduardo <linco.on@hotmail.com>
2025-07-03 09:46:56 -0300

    trocar gitlab pt2

--- pesquisa.js
+++ pesquisa.js
@@ -3,15 +3,15 @@ function abreJanela() {
     url = document.getElementById("url").value;
     combo = document.getElementById("combo").selectedIndex;
 
-    var gitlab = ('https://gitlab.com/secullum/sec-issues/-/issues/?sort=closed_at_desc&state=opened&search='+ url'&first_page_size=100');
+    var gitlabLink = ('https://gitlab.com/secullum/sec-issues/-/issues/?sort=closed_at_desc&state=opened&search='+ url'&first_page_size=100');
     var caseLink = ('http://bugtracker.secullum.com.br/edit_bug.aspx?id=' + url);
     var pfLink = ('https://www.secullum.com.br/pf?id=' + url);
 
     if (combo == 0) {
-        window.open(caseLink);
+        window.open(gitlabLink);
     }
     if (combo == 1) {
-        window.open(gitlab);
+        window.open(caseLink);
     }
     if (combo == 2) {
         window.open(pfLink);

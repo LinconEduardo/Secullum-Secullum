754484cff989c2b7d260f80cabd64cd0bdc707d8
LinconEduardo <linco.on@hotmail.com>
2025-07-03 09:55:14 -0300

    arrumar url

--- pesquisa.js
+++ pesquisa.js
@@ -3,9 +3,9 @@ function abreJanela() {
     url = document.getElementById("url").value;
     combo = document.getElementById("combo").selectedIndex;
 
-    var gitlabLink = ('https://gitlab.com/secullum/sec-issues/-/issues/?sort=closed_at_desc&state=opened&search='+ url'&first_page_size=100');
+    var gitlabLink = ('https://gitlab.com/secullum/sec-issues/-/issues/?sort=closed_at_desc&state=opened&search='+ url + '&first_page_size=100');
     var caseLink = ('http://bugtracker.secullum.com.br/edit_bug.aspx?id=' + url);
-    var pfLink = ('https://www.secullum.com.br/pf?id=' + url);
+    var pfLink = ('https://www.secullum.com.br/pt/canal-cliente/perguntas/' + url);
 
     if (combo == 0) {
         window.open(gitlabLink);
